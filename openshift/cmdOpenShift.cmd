1a5448875446eddf7d5224821c3e0f30435e9c1f
seancool61 <seancool61@hotmail.com>
2020-05-25 14:03:09 +0800

    openshift

--- openshift/cmdOpenShift.cmd
+++ openshift/cmdOpenShift.cmd
@@ -7,8 +7,16 @@ rem     Only include specified layers when exporting. Layers can be specified ei
 rem C:\Progra~2\Dia\bin\diaw.exe OpenShift.dia -t png --size=1024x --show-layers=background,1 --export=images\openshift-02.png
 rem C:\Progra~2\Dia\bin\diaw.exe OpenShift.dia -t png --size=1024x --show-layers=background,1-2 --export=images\openshift-03.png
 
-for /L %%a in (1,1,7) do (
-	set "layerrange=Background,1-%%a"
+for /L %%a in (1,1,3) do (
+	set "layerrange=Background,%%a"
+	set "filename=images\dia_openshift_%%a.png"
+	echo .....layerrange !layerrange! filename !filename!
+	rem C:\Progra~2\Dia\bin\diaw.exe OpenShift.dia -t png --size=512x --show-layers=!layerrange! --export=!filename!
+	C:\Progra~2\Dia\bin\diaw.exe OpenShift.dia -t png --size=4096x --show-layers=!layerrange! --export=!filename!
+)
+
+for /L %%a in (4,1,10) do (
+	set "layerrange=Background,4-%%a"
 	set "filename=images\dia_openshift_%%a.png"
 	echo .....layerrange !layerrange! filename !filename!
 	rem C:\Progra~2\Dia\bin\diaw.exe OpenShift.dia -t png --size=512x --show-layers=!layerrange! --export=!filename!
